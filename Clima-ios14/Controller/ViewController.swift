8e0e452193fe3291e4df74ba6ae9b5b2c1af48a1
appfromape <appfromape@gmail.com>
2021-02-26 15:38:10 +0800

    add some UITextFieldDelegate function.

--- Clima-ios14/Controller/ViewController.swift
+++ Clima-ios14/Controller/ViewController.swift
@@ -7,18 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var tempLable: UILabel!
     @IBOutlet weak var cityLable: UILabel!
+    @IBOutlet weak var searchTextFiled: UITextField!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchTextFiled.delegate = self
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextFiled.endEditing(true)
+        print(searchTextFiled.text!)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextFiled.endEditing(true)
+        print(searchTextFiled.text!)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if searchTextFiled.text != "" {
+            return true
+        } else {
+            searchTextFiled.placeholder = "打些什麼吧"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextFiled.text = ""
+    }
+    
 }
 
